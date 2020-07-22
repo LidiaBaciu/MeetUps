5a8926462acd7c0718b1092ca6135b83bde800e2
LidiaBaciu <lidia.baciu97@gmail.com>
2020-07-22 16:08:17 +0200

    used SpringBoot Initializr to create the project
    Project: Maven
    Language: Java
    Spring Boot: 2.3.1
    Java: 8
    Dependencies: Spring Gata JPA, Spring Security, Spring Web and MySQL Driver

---
+++ meetups-server/src/main/java/com/example/meetups/MeetupsApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupsApplication.class, args);
+	}
+
+}
---
+++ meetups-server/src/test/java/com/example/meetups/MeetupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
